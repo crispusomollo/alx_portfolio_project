762d3b9b77b336ac73588997ecea892e52aeb4fa
crispusomollo <crispusomollo@gmail.com>
2024-05-21 15:52:05 +0300

    MongoDB connection string

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+//Midlleware for parsing JSON
 app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/bookapi', { useNewUrlParser: true, useUnifiedTopology: true})
+
