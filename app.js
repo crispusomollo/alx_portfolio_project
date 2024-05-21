1c448f7a7182a98d5647420c1cc08447aef6f95d
crispusomollo <crispusomollo@gmail.com>
2024-05-21 16:21:20 +0300

    Created a model for the book

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ app.use(json())
 
 connect('mongodb://localhost:27017/bookapi', { useNewUrlParser: true, useUnifiedTopology: true})
 
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    publishedYear: Number,
+    notes: String,
+    reviews: String
+})
+
+
+const Book = mongoose.model('Book', bookSchema)
