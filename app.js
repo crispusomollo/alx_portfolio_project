dbb6efd8a2578c8b54f07bdb8a298d23f772f03a
crispusomollo <crispusomollo@gmail.com>
2024-05-21 16:35:42 +0300

    return info of the port the server is listening on

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
+app.listen(port, () => {
+    console.log('Server running on http://localhost:${port}')
+});
 
 //Midlleware for parsing JSON
-app.use(json())
+app.use(json());
 
-connect('mongodb://localhost:27017/bookapi', { useNewUrlParser: true, useUnifiedTopology: true})
+connect('mongodb://localhost:27017/bookapi', { useNewUrlParser: true, useUnifiedTopology: true});
 
 const bookSchema = new mongoose.Schema({
     title: String,
@@ -16,7 +19,6 @@ const bookSchema = new mongoose.Schema({
     publishedYear: Number,
     notes: String,
     reviews: String
-})
+});
 
-
-const Book = mongoose.model('Book', bookSchema)
+const Book = mongoose.model('Book', bookSchema);
