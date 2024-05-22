c5332faf200b77f4fe053465cf51e6634bf14a3a
crispusomollo <crispusomollo@gmail.com>
2024-05-22 10:32:51 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+//const { MongoClient } = require('mongodb');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 app.listen(port, () => {
     console.log('Server running on http://localhost:${port}')
 });
@@ -12,13 +12,13 @@ app.listen(port, () => {
 //Midlleware for parsing JSON
 app.use(json());
 
-//connect('mongodb://localhost:27017/bookapi', { useNewUrlParser: true, useUnifiedTopology: true});
+connect('mongodb://localhost:27017/bookapi', { useNewUrlParser: true, useUnifiedTopology: true});
 // Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
+//const url = 'mongodb://localhost:27017';
+//const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'bookapi';
+//const dbName = 'bookapi';
 
 const bookSchema = new mongoose.Schema({
     title: String,
